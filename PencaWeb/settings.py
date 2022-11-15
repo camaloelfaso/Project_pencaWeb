24ad8d5474f603276ed4421451175c0339ab5b50
camaloelfaso <camilofasano89@gmail.com>
2022-11-14 23:12:16 -0300

    update allowed hosts

--- PencaWeb/settings.py
+++ PencaWeb/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,7 +30,9 @@ SECRET_KEY = 'django-insecure-fp%7l*@&buj3i52+%naxtzt)*8cdjc861jernwf8kvjcrl)4u)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 
+    "penca.delebru.com" 
+]
 
 
 # Application definition
